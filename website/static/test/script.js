d10eb33040efbc4b8c38904f2fb373fd1493ba19
minhquang5014 <minhquang5014@gmail.com>
2024-09-11 00:15:38 +0700

    fix the js file

--- website/static/test/script.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.getElementById('toggleBtn');
-const sidebar = document.getElementById('navbar');
-const mainContent = document.getElementById('main');
-
-toggleBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('collapsed');
-  mainContent.classList.toggle('shifted');
-});
