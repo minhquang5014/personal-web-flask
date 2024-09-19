a72a1209b3d16ab2abab59e978de5fa9b59e25f7
minhquang5014 <minhquang5014@gmail.com>
2024-09-19 09:36:41 +0700

    modify the base template and views.py

--- website/views.py
+++ website/views.py
@@ -4,17 +4,21 @@ from flask_login import login_required
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
     return render_template("home.html")
 
 @views.route('/ml', methods=['GET', 'POST'])
+@login_required
 def machine_learning():
     return render_template("ml.html")
 
 @views.route('/other-achievements', methods=['GET', 'POST'])
+@login_required
 def other_achievements():
     return render_template("other_achievements.html")
 
 @ views.route('/computer-vision', methods=['GET', 'POST'])
+@login_required
 def contact():
     return render_template("computer_vision.html")
