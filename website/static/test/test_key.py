85d02431a58db7067eb9afd18cabf6ad3c1f074a
minhquang5014 <minhquang5014@gmail.com>
2024-10-07 22:44:09 +0700

    Writing to the spreadsheet successfully

--- website/static/test/test_key.py
+++ website/static/test/test_key.py
@@ -14,13 +14,24 @@ client = gspread.authorize(credentials)
 
 # Open the Google Sheet by name
 sheet_name = 'database'  # Replace with your sheet's name
+
+def check_available_row():
+    """Filtering out the rows that return None to find out the next empty row
+    Once an empty row has been determined, we can write down the new info, without overwriting the already existing rows"""
+    pass
+
 try:
     sheet = client.open(sheet_name).sheet1
     print(f"Successfully connected to {sheet_name}")
+
+    list_of_hashes = sheet.get_all_records()
+    print(list_of_hashes)
     
     # Test reading a value
     cell_value = sheet.cell(1, 1).value 
     print(f"The value in A1 is: {cell_value}")
+
+    # sheet.update_cell(2, 1, "minhquang5014@gmail.com")
 
 except Exception as e:
     print(f"Unable to connect to the Google Sheet: {e}")
