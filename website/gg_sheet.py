fe89dc943cc00dd5cae0e167c33dcf67df082bf3
minhquang5014 <minhquang5014@gmail.com>
2024-10-13 23:09:12 +0700

    encoding the jsonformat into base64 string to see if it works

--- website/gg_sheet.py
+++ website/gg_sheet.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
-
+import json
+import base64
 class GoogleSheetClient:
     """
     This class is for connecting with the gg spreadsheet and writing down data
@@ -9,13 +10,21 @@ class GoogleSheetClient:
         """Initializing everything"""
         self.SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
                        'https://www.googleapis.com/auth/drive']
-        self.credentials_file = credentials_file
-        self.sheet_name = sheet_name
+        self.file = self.create_json_from_env(credentials_file)
+        self.sheetname = sheet_name
         self.client = self.authenticate()
     def authenticate(self):
         """Authenticate and return a gspread client"""
-        credentials = Credentials.from_service_account_file(self.credentials_file, scopes = self.SCOPES)
+        credentials = Credentials.from_service_account_file(self.file, scopes = self.SCOPES)
         return gspread.authorize(credentials)
+    def create_json_from_env(self, credentials_file):
+        """decoding all the json formats into environment variables"""
+        credential_json = base64.b64decode(credentials_file).decode('utf-8')
+        # so vercel doesn't support uploading sensitive files such as json keys, so instead you'll have to decode and store everything in a json file in the cloud server
+        path = '/website/key.json'
+        with open(path, "w") as f:
+            f.write(credential_json)
+        return path
     def write_in4_to_spreadsheet(self, email, username, datetime):
         try:
             sheet = self.client.open(self.sheet_name).sheet1
@@ -23,12 +32,12 @@ class GoogleSheetClient:
             # this is running much faster and more efficiently
             sheet.append_row([email, username, datetime])
 
-            # the method of calculating manually which row the data should be written on is so inefficient 
+            """ugly code - the method of calculating manually which row the data should be written on is so inefficient 
             # email_column = sheet.col_values(1)
             # next_line = len(email_column) + 1
             # sheet.update_cell(next_line, 1, email)
             # sheet.update_cell(next_line, 2, username)
-            # sheet.update_cell(next_line, 3, datetime)
+            # sheet.update_cell(next_line, 3, datetime)"""
 
         except Exception as e:
             # print out the exception in here for debugging
